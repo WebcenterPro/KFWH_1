40e3b5c9eceaa15c93ed61a46a9c2b95947067f9
R45H <gmr45h@gmail.com>
2016-10-10 12:07:23 +0300

    Added a script for the arrows in the main menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
+/* ===== JavaScript ===== */
+
+document.addEventListener("DOMContentLoaded", ready);
+
+function ready() {
+
+	/* Стрелочки главного меню */
+	var items = document.querySelector(".header_main__items");
+	var listComp = getComputedStyle(items);
+
+	items.onclick = function(event) {
+		var target = event.target;
+		if (listComp.display != "block") return;
+		if ((target.parentNode.childNodes.length == 3) &&
+			(!target.classList.contains("glyphicon"))) return;
+		
+		var glyph = target;
+		
+		if (glyph.classList.contains("glyphicon")){
+			target = target.parentNode;
+		}
+		
+		target.classList.add("header_main__active");
+		var arrow = document.querySelector(".header_main__active .header_main__arrow_down");
+		arrow.classList.toggle("glyphicon-chevron-down");
+		arrow.classList.toggle("glyphicon-chevron-up");
+		target.classList.remove("header_main__active");
+	}
+
+}
+
+
+/* ===== JQUERY ===== */
+
 /* Слайдер на главной */
 $(document).ready(function(){
 	$(".owl").owlCarousel({
@@ -11,4 +45,3 @@ $(document).ready(function(){
 		dotClass: "owl__dot"
 	});
 });
-/* ================== */
