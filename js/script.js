50f63462fa10fa826bb23378b5c17d2c07da1a22
R45H <gmr45h@gmail.com>
2016-10-10 23:52:06 +0300

    Complete a dropdown window for the wide unit_top block

--- js/script.js
+++ js/script.js
@@ -1,31 +1,79 @@
-/* ===== JavaScript ===== */
+/* ===== JAVASCRIPT ===== */
 
 document.addEventListener("DOMContentLoaded", ready);
 
 function ready() {
 
 	/* Стрелочки главного меню */
-	var items = document.querySelector(".header_main__items");
-	var listComp = getComputedStyle(items);
 
-	items.onclick = function(event) {
-		var target = event.target;
-		if (listComp.display != "block") return;
-		if ((target.parentNode.childNodes.length == 3) &&
-			(!target.classList.contains("glyphicon"))) return;
-		
-		var glyph = target;
-		
-		if (glyph.classList.contains("glyphicon")){
-			target = target.parentNode;
+	document.querySelector(".header_main__items").onclick = function(event) {
+
+		var glyph = document.querySelector(".header_main__item.open .header_main__link .glyphicon");
+		if (glyph) {
+			glyph.classList.add("glyphicon-chevron-down");
+			glyph.classList.remove("glyphicon-chevron-up");
+			glyph.removeAttribute("data-active");
+
+			//alert("Закрывается");
+		} else {
+			setTimeout(function() {
+				glyph = document.querySelector(".header_main__item.open .header_main__link .glyphicon");
+				if (!glyph) return;
+				glyph.classList.remove("glyphicon-chevron-down");
+				glyph.classList.add("glyphicon-chevron-up");
+				glyph.setAttribute("data-active", "true");
+				//alert("Открывается");
+			}, 0);
+
 		}
-		
-		target.classList.add("header_main__active");
-		var arrow = document.querySelector(".header_main__active .header_main__arrow_down");
-		arrow.classList.toggle("glyphicon-chevron-down");
-		arrow.classList.toggle("glyphicon-chevron-up");
-		target.classList.remove("header_main__active");
+
+		var lastItem = event.target;
 	}
+
+	/* ======================= */
+
+	/* Выезжающая текстовая панель в блоке UNIT_TOP */
+
+	var inner = null;
+	var parent = null;
+	var arrow = null;
+
+	document.querySelector(".unit_top__content--wide").onmouseenter = function(event) {
+
+		var target = event.target;
+		inner = target.children[0];
+
+		inner.oldHeight = parseInt(getComputedStyle(inner).height);
+		inner.style.maxHeight = "initial";
+		inner.newHeight = parseInt(getComputedStyle(inner).height);
+
+		if (inner.oldHeight >= inner.newHeight) {
+			inner.style.maxHeight = "";
+			return;
+		}
+
+		inner.resHeight = inner.oldHeight - inner.newHeight;
+		target.style.transform = "translateY(" + inner.resHeight + "px)";
+
+		parent = target.parentElement;
+		parent.height = parseInt(getComputedStyle(parent).height);
+		parent.style.height = (parent.height + inner.resHeight) + "px";
+
+		arrow = inner.lastElementChild;
+		arrow.classList.remove("glyphicon-chevron-down");
+		arrow.classList.add("glyphicon-chevron-up");
+	}
+
+	document.querySelector(".unit_top__content--wide").onmouseleave = function(event) {
+		var target = event.target;
+		inner.style.maxHeight = "";
+		target.style.transform = "";
+		parent.style.height = "";
+		arrow.classList.remove("glyphicon-chevron-up");
+		arrow.classList.add("glyphicon-chevron-down");
+	}
+
+	/* ============================================ */
 
 }
 
